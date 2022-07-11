d0021490d6552a0b863c1d11c48f2f2a15b3557b
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-07-11 22:46:17 +0530

    learning string function of python

---
+++ july22/week_2/day_1/learningpyday2.py
@@ -0,0 +1,27 @@
+from ast import Module
+import curses
+from curses.ascii import isalnum
+from curses.panel import _Curses_Panel
+from msilib import type_key
+from tkinter import N
+
+
+
+sujit= "hello sujit"
+# sujit3=" 4 "
+# sujit1=" 79 "
+# sujit2=37.4
+# print( 100 * str(int( sujit3) + int(sujit1)))
+# print((sujit)(sujit1) (sujit2) )
+# str()
+# int()
+# float() this are use for change type
+# print(10 *" sujit\n")
+# print("enter your number")
+# cal=input()
+# col=input()
+# print(float(cal) / int(col))
+# sujit="sujit is a good boy"
+# print(len(sujit))
+# print(sujit[-`8::p-7])  
+print(sujit.endswith(boy))
