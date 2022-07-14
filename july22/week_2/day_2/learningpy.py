d4f0d28b3a1232dca4a65b2716e6768b98e0d9c4
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-07-14 12:09:44 +0530

    test commit

---
+++ july22/week_2/day_2/learningpy.py
@@ -0,0 +1,27 @@
+# grocery=[ "harpic","vimliquid","dio","chips", 56]
+# print(grocery)
+# numbers=[1,5,3,4,2]
+# numbers.sort()
+# numbers.reverse()
+# print(numbers[2])
+# numbers.append(7)
+# numbers.insert(4,89)
+# numbers.remove(5)
+# numbers.pop()
+# mutable=can change
+# immutable=can not change
+# tp=(1,2,3)
+# tp[1]=89
+# print(tp)
+from re import A
+from tempfile import tempdir
+
+
+a=8
+b=7
+# a,b=b,a
+temp=a
+a=b
+b=temp
+print(a,b)
+    
---
+++ july22/week_2/day_4/pythonlearning.py
@@ -0,0 +1,17 @@
+# dictinary in nothing but key value pairs
+# from locale import DAY_1
+# dl=()
+# print(type(dl))
+d2={"sujit":"chiken", "hen":"grains", "horse":"chana", "chadu":{"b":"sandwitch","lunch":"roti","d":"chiken"}}
+# d2["ankit"]="chakna"
+# d2["vaishnav"]="butter"
+# # print(d2 ["chadu"]["d"])
+# del d2["vaishnav"]
+# d3=d2.copy()
+# del d3["sujit"]
+# d2["leena"]="horse"
+# d2.update({"leena":"horse"})
+# print(d2)
+# print(d2.get("sujit"))
+# print(d2.keys())
+print(d2.items())
