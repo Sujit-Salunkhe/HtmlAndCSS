5e270dde3055368cbf831abbe703234d41a37388
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2022-07-17 17:46:25 +0530

    practising paython from w3 website

---
+++ july22/week_4/paython.py
@@ -0,0 +1,17 @@
+# a=" big ,number "
+# b=a.split(",")
+# # print(b)
+# amt=4
+# price=5
+# qut=10
+# myorder="my amt {}  your price{} for {} rs is."
+# print(myorder.format(amt,price,qut))
+# print(4<5)
+veg=["mirchi","onion","potato"]
+nonveg=["chiken","goat"]
+# veg.insert(2,"cook")
+# veg[1:3]=["chiken" ]
+veg.extend(nonveg)
+
+
+print(veg)
