28f0b0cdff33aa9c60b3dc2030fe656a46ccd872
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2024-02-21 19:57:28 +0530

    changing in calculater ui

---
+++ july23/week_3/day_3/script.js
@@ -0,0 +1,12 @@
+// function countdown(){
+//     let time =document.getElementsByClassName("time")[0];
+//     let seconds= 30
+//     let countdowninterval = () =>{
+//         if( seconds > 0 ){
+//             time.textContent="Time left" + seconds
+//             seconds--;
+//         }
+        
+//     }
+//     // , 1000);};
+// countdown();
